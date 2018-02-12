afa580ae1c3b0fc68cae949d74ae71cbc5cb81d2
Rafał Kociński <rafal.kocinski@infinityworks.com>
2018-02-12 09:49:06 +0000

    [RPS-380] CI migration: sanitise duplicate paths
    Node names of a few known datasets now get suffixes appended
    to make their paths unique and thus ensure all are imported.

--- src/main/java/uk/nhs/digital/ps/migrator/report/DatasetMigrationImpact.java
+++ src/main/java/uk/nhs/digital/ps/migrator/report/DatasetMigrationImpact.java
@@ -3,11 +3,11 @@ package uk.nhs.digital.ps.migrator.report;
 public enum DatasetMigrationImpact {
 
     FIELD_MIGRATED_AS_IS("Field migrated as is"),
-    FIELD_MIGRATED_MODIFIED("Modified value migrated"),
     FIELD_NOT_MIGRATED("Field not migrated"),
     FIELD_MIGRATED_PARTIALLY("Field migrated partially"),
+    VALUE_MIGRATED_MODIFIED("Modified value migrated"),
     DATASET_NOT_MIGRATED("Dataset not migrated"),
-    ONE_DUPLICATE_DATASET_MIGRATED("Only one of duplicates migrated");
+    ONE_DUPLICATE_DATASET_MIGRATED("Only one duplicate migrated");
 
     private final String description;
 
